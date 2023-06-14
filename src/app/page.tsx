70690c6e2479d509b7f1950dc40b6e928e921c89
matiasmjcs <matiasmejiascisternas@gmail.com>
2023-06-14 11:30:19 -0300

    update tittle recurses

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import { Footer } from '@/components/footer/footer'
 import Header from '@/components/header/header'
 import { Main } from '@/components/main/main'
-import Nav from '@/components/nav/nav'
 
 export default function Home() {
   return (
     <>
-    <Nav/>
     <Header/>
     <Main/>
     <Footer/>
