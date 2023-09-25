10a28d7be40c38d683a66390d863b571dbed9240
matiasmjcs <matiasmejiascisternas@gmail.com>
2023-09-25 17:26:14 -0300

    update projects components

--- src/components/projects-component.tsx
+++ src/components/projects-component.tsx
@@ -2,18 +2,18 @@ import Link from 'next/link';
 import React from 'react';
 import { FaGithub } from 'react-icons/fa';
 
-export const ProyectComponent = ({ name, route, github, description }: { name: string; route: string; github: string; description: string }) => {
+export const ProjectsComponent = ({ name, route, github, description }: { name: string; route: string; github: string; description: string }) => {
   return (
     <article className="grid dark:bg-gray-900 justify-items-start rounded-lg bg-color3 p-4">
-      <h3 className="flex items-center">
+      <h3 className="flex items-center w-full justify-between">
         <Link className="text-rose-600 text-lg font-semibold" href={route}>
           {name}
         </Link>
-        <Link href={`https://github.com/matiasmjcs/${github}`} target="_blank">
+        <Link className='justify-self-end' href={`https://github.com/matiasmjcs/${github}`} target="_blank">
           <FaGithub className="text-white ml-5" />
         </Link>
       </h3>
-      <p className="text-start font-medium">{description}</p>
+      <p className="text-start text-base font-medium">{description}</p>
     </article>
   );
 };
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { repositories } from '@/resources/repositories';
-import { ProyectComponent } from './proyect-component'; 
+import { ProjectsComponent } from './projects-component'; 
 
 export const Project = () => {
   return (
@@ -8,7 +8,7 @@ export const Project = () => {
       <h3 className="text-white text-xl mb-6 self-start font-bold">Explore Some of My Personal Projects</h3>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
         {repositories.map((repo) => (
-          <ProyectComponent
+          <ProjectsComponent
             name={repo.name}
             route={repo.route}
             github={repo.github}
