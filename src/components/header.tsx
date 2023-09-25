4f98fd6170c340683ea9d76c80b809348f502c34
matiasmjcs <matiasmejiascisternas@gmail.com>
2023-09-25 17:16:44 -0300

    update error styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 const Header = (): JSX.Element => {
   return (
-    <header className="w-full h-96 grid grid-cols-1 md:grid-cols-2 items-center justify-center mx-[16px] md:p-20">
+    <header className="w-full h-96 grid grid-cols-1 md:grid-cols-2 items-center justify-center px-[1rem] md:p-20">
       <div>
         <h2 className="text-2xl font-bold">
           Hi! I am Matías
